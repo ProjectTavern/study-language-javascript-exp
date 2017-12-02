cfe1e9c232c73532c9177f4af6da9a9e53806d5e
Arth <gift8128@naver.com>
2017-12-02 17:40:46 +0900

    .

--- TypeStudy.ts
+++ TypeStudy.ts
@@ -3,8 +3,12 @@ declare var $:{
 };
 
 class Point {
-    constructor(public x: number, public y: number){
+    x: number;
+    y: number;
 
+    constructor(x: number,y: number){
+        this.x = x;
+        this.y = y;
     }
 
     add(point: Point){
@@ -15,3 +19,22 @@ class Point {
 const p1 = new Point(0, 10);
 const p2 = new Point(10, 20);
 let nextPoint = p1.add(p2);
+
+/* Equality */
+var foo = {};
+var bar = foo;
+var bar = {};
+
+console.log("Hello, world!");
+
+class Point3D extends Point {
+    z: number;
+    constructor(x: number, y: number, z: number){
+        super(x, y);
+        this.z = z;
+    }
+    add(point: Point3D) {
+        var point2D = super.add(point);
+        return new Point3D(point2D.x, point2D.y, this.z + point.z);
+    }
+}
