ad3db84f13762a902a2729f4a87e93b9c9590ac7
qoov <emrys8128@gmail.com>
2018-05-12 19:03:26 +0900

    [qoov] 숫자 찾기

--- RegExp/findStringNumber.js
+++ RegExp/findStringNumber.js
@@ -1,10 +1,10 @@
-var searchTarget = "123-4567-8992";
+const searchTarget = "123-4567-8992";
 
 /* js에서는 g를 붙이지 않으면 첫회에 찾은 것에서 멈추고 해당 데이터와 인덱스, 인풋값을 배열의 형태로 리턴해준다. */
-var regExp = /\d+/;
+let regExp = /\d+/;
 
 /* 따라서 모든 곳을 조회하고 싶을 때는 g를 붙여주고 찾아야만 한다. */
-var regExpG = /\d+/g;
+let regExpG = /\d+/g;
 
 console.log(searchTarget.match(regExp));
 
