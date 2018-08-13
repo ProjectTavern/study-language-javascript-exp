34a51ed2456de1e4da6767d6b08662e001e08981
qoov <emrys8128@gmail.com>
2018-08-14 01:28:20 +0900

    for-in statementˆ

---
+++ VanillaLab/legacy/forin.js
@@ -0,0 +1,12 @@
+var forin = {
+  name : "lina",
+  age : 25,
+  hp : 1050,
+  mana : 3750,
+  level : 20
+};
+
+/* iterating property name */
+for (var propName in forin) {
+  console.log(propName);
+}
